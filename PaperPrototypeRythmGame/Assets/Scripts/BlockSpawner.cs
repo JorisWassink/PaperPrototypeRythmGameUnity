e04090044c14564c8d3425d9e2ebb3dbf0b8af99
Joris <547363@student.saxion.nl>
2025-05-15 10:36:16 +0200

    Added customizability
    block spawning should now be way easier

---
+++ PaperPrototypeRythmGame/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Serialization;
+
+[System.Serializable]
+public struct NoteList
+{
+    public GameObject note;
+    public float InitialDelay;
+    public float spawnInterval;
+    public int spawnCount;
+}
+
+public class BlockSpawner : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private Material blockMaterial;
+    [SerializeField] private NoteList[] noteList;
+
+    void Start()
+    {
+        StartCoroutine(SpawnBlocks());
+        if (spawnPoint == null)
+            spawnPoint = transform;
+    }
+
+    IEnumerator SpawnBlocks()
+    {
+        foreach (var note in noteList)
+        {
+            yield return new WaitForSeconds(note.InitialDelay);
+
+            for (int i = 0; i < note.spawnCount; i++)
+            {
+                SpawnBlock(note.note);
+                yield return new WaitForSeconds(note.spawnInterval);
+            }
+        }
+    }
+
+    void SpawnBlock(GameObject note)
+    {
+        var spawnedNote = Instantiate(note, spawnPoint.position, spawnPoint.rotation, transform);
+        spawnedNote.GetComponent<Renderer>().material = blockMaterial;
+    }
+}
